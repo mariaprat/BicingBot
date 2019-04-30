e64fdc1f9e2ee601f90dfcc2f3cfdfaf439dfb30
Max Balsells <maxbalsells@MacBook-Air-de-Max.local>
2019-04-30 18:28:07 +0200

    Adding files

---
+++ bot.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+import networkx as nx
+import geopy
+import haversine
+import staticmap
+import python-telegram-bot
+import numpy as np #try not to use it 
+
---
+++ data.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+import networkx as nx
+import geopy
+import haversine
+import staticmap
+import python-telegram-bot
+import numpy as np #try not to use it 
+
+def dijkstra(nx.Graph):
+
+	
+
