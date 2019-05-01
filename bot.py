6cc37b2e4f20ca103be876b8e746a9d6ae94533f
Max Balsells <maxbalsells@MacBook-Air-de-Max.local>
2019-05-01 19:08:59 +0200

    Updating everything

--- bot.py
+++ bot.py
@@ -1,28 +1,44 @@
 import telegram
+import networkx as nx
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from data import precalculation
 from data import geometric_graph
 
 def start(bot, update):
+    global edge_list
+    global G
     bot.send_message(chat_id = update.message.chat_id, text = "Hello!")
-    return precalculation()
+    edge_list, G = precalculation()
+
 
 def authors(bot, update):
+    global edge_list
     bot.send_message(chat_id = update.message.chat_id, text = "My creators are Maria Prat and Max Balsells (I dont know their official email yet)")
-
-def graf(bot, update, args):
-	global edge_list
-	distance = int(args[0])
-
-	G = geometric_graph(edge_list)
-	bot.send_message(chat_id = update.message.chat_id, text = "okay")
-
-	return G
 
 
 def nodes(bot, update):
+	global G
 	bot.send_message(chat_id = update.message.chat_id, text = G.number_of_nodes())
+
+def edges(bot, update):
+	global G
+	bot.send_message(chat_id = update.message.chat_id, text = G.number_of_edges())
+
+
+def graph(bot, update, args):
+    try:
+        x = int(args[0])
+        global edge_list
+        global G
+
+        G = geometric_graph(edge_list, x, G)
+
+        bot.send_message(chat_id=update.message.chat_id, text= "ok")
+
+    except Exception as e:
+        print(e)
+        bot.send_message(chat_id=update.message.chat_id, text='💣')
 
 
 
@@ -31,10 +47,14 @@ TOKEN = open('token.txt').read().strip()
 updater = Updater(token = TOKEN)
 dispatcher = updater.dispatcher
 
-edge_list = dispatcher.add_handler(CommandHandler('start', start))
+edge_list = list()
+G = nx.Graph()
+
+dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('authors', authors))
-G = dispatcher.add_handler(CommandHandler('graf', graf, pass_args = True))
-dispatcher = dispatcher.add_handler(CommandHandler('nodes', nodes))
+dispatcher.add_handler(CommandHandler('nodes', nodes))
+dispatcher.add_handler(CommandHandler('edges', edges))
+dispatcher.add_handler(CommandHandler('graph', graph, pass_args = True))
 
 
 updater.start_polling()
--- data.py
+++ data.py
@@ -8,30 +8,31 @@ def precalculation():
     url = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information'
     bicing = DataFrame.from_records(pd.read_json(url)['data']['stations'], index='station_id')
     
+    G = nx.Graph()
     edge_list = list()
 
     for i in bicing.itertuples(): 
+        G.add_node(i.Index)
         for j in bicing[bicing.index > i.Index].itertuples():
     	    edge_list.append((haversine((i.lat, i.lon), (j.lat, j.lon)), i.Index, j.Index))
 
     edge_list.sort()
-    return edge_list
 
-def geometric_graph(edge_list, d):
-	print("here we are")
-	print(len(edge_list))
+    return edge_list, G
 
+def geometric_graph(edge_list, d, F):
 	G = nx.Graph()
+	G.add_nodes_from(F.nodes)
 
 	i = 0
 	while edge_list[i][0] < d/1000:
-		print(i)
+		i = i + 1
 		G.add_edge(edge_list[i][1], edge_list[i][2], weight = edge_list[i][0])
 
-	print(G.number_of_nodes())
 	return G
 
-#A = precalculation()
+
+#A = precalculation(G)
 #G = geometric_graph(A, 500)
 
 # This function constructs a geometic graph given the distance d
