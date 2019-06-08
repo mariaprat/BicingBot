1d7f4845b980f0d29698e243ce3d665d3d3dc0be
mbalsells <43616135+mbalsells@users.noreply.github.com>
2019-06-08 21:42:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telegram
+import os
 from geopy.geocoders import Nominatim
 import networkx as nx
 from telegram.ext import Updater
@@ -6,8 +7,6 @@ from telegram.ext import CommandHandler
 from staticmap import StaticMap, Line
 
 from data import *
-
-#user_data (edge_list, G, diccionari)
 
 def start(bot, update, user_data):
     name =  update.message.chat.first_name
@@ -19,11 +18,11 @@ def authors(bot, update):
     message2 = "Their emails are respectively maria.prat@est.fib.upc.edu and max.balsells.i@est.fib.upc.edu. "
     bot.send_message(chat_id = update.message.chat_id, text = message1 + message2)
     
-def help(bot, update, user_data):
+def helpp(bot, update):
     initial = "This bot helps you to use Bicing in Barcelona."
     start = "Use /start to get a welcome message."
     authors = "Use /authors to get information about my authors."
-    graph = "Use /graph <distance> to initialize a new graph with up-to-date data. Two stations will be connected the distance between them is less than <distance> (in meters).
+    graph = "Use /graph <distance> to initialize a new graph with up-to-date data. Two stations will be connected the distance between them is less than <distance> (in meters)."
     nodes = "Use /nodes to get the number of stations in the current graph."
     edges = "Use /edges to get the number of connections between stations in the current graph."
     components = "Use /components to get the number of connected components in the current graph."
@@ -31,6 +30,7 @@ def help(bot, update, user_data):
     route = "Use /route <origin>, <destiny> to create a map with the faster route between <origin> and <destiny>."
     message = initial + start + authors + graph + nodes + edges + components + plotgraph + route
     bot.send_message(chat_id = update.message.chat_id, text = message)
+
 
 def nodes(bot, update, user_data):
     bot.send_message(chat_id = update.message.chat_id, text = user_data['G'].number_of_nodes())
@@ -41,22 +41,21 @@ def components(bot, update, user_data):
 def edges(bot, update, user_data):
     bot.send_message(chat_id = update.message.chat_id, text = user_data['G'].number_of_edges())
 
-def plotgraph(bot, update, user_data):
-    fitxer = 'map.png'
 
-    m = ploting(user_data['G'], user_data['diccionari'])
+def plotgraph(bot, update, user_data):
+    fitxer = "plotgraph_" + str(update.message.chat_id) + ".png"
+
+    m = ploting(user_data['G'], user_data['position'])
     image = m.render(zoom = 12)
+
     image.save(fitxer)
 
     bot.send_photo(chat_id=update.message.chat_id, photo=open(fitxer, 'rb'))
     os.remove(fitxer)
 
-
 def graph(bot, update, args, user_data):
     try:
-        x = int(args[0])
-
-        user_data['G'] = geometric_graph(user_data['edge_list'], x, user_data['G'])
+        user_data['G'], user_data['position'], user_data['bicing'], user_data['bikes'] = geometric_graph(int(args[0]))
         bot.send_message(chat_id=update.message.chat_id, text= "ok")
 
     except Exception as e:
@@ -65,17 +64,26 @@ def graph(bot, update, args, user_data):
 
 def route(bot, update, args, user_data):
     try:
-        x = " ".join(args)
+        fitxer = "route_" + str(update.message.chat_id) + ".png"
 
-        fitxer = 'map1.png'
-
-        m = ruta(x, user_data['G'], user_data['diccionari'])
+        m, time = ruta(" ".join(args), user_data['G'], user_data['position'])
         image = m.render(zoom = 12)
         image.save(fitxer)
 
         bot.send_photo(chat_id=update.message.chat_id, photo=open(fitxer, 'rb'))
-        bot.send_message(chat_id=update.message.chat_id, text= "ok")
-        #os.remove(fitxer) whyyyyy
+        bot.send_message(chat_id=update.message.chat_id, text= "If you follow this rute you will arrive at your destination in {} minutes".format(time))
+        os.remove(fitxer)
+        
+
+    except Exception as e:
+        print(e)
+        bot.send_message(chat_id=update.message.chat_id, text='💣')
+
+def distribute(bot, update, args, user_data):
+    try:
+        cost, information = distribution(int(args[0]), int(args[1]), user_data['G'], user_data['bicing'], user_data['bikes'])
+        bot.send_message(chat_id=update.message.chat_id, text= "Total cost of transferring bicycles: {} km".format(cost))
+        bot.send_message(chat_id=update.message.chat_id, text= information)
 
     except Exception as e:
         print(e)
@@ -88,6 +96,7 @@ updater = Updater(token = TOKEN)
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('start', start, pass_user_data=True))
+dispatcher.add_handler(CommandHandler('help', helpp))
 dispatcher.add_handler(CommandHandler('authors', authors))
 dispatcher.add_handler(CommandHandler('nodes', nodes, pass_user_data=True))
 dispatcher.add_handler(CommandHandler('edges', edges, pass_user_data=True))
@@ -95,6 +104,7 @@ dispatcher.add_handler(CommandHandler('plotgraph', plotgraph, pass_user_data=Tru
 dispatcher.add_handler(CommandHandler('components', components, pass_user_data=True))
 dispatcher.add_handler(CommandHandler('graph', graph, pass_args = True, pass_user_data=True))
 dispatcher.add_handler(CommandHandler('route', route, pass_args = True, pass_user_data=True))
-dispatcher.add_handler(CommandHandler('help', route))
+dispatcher.add_handler(CommandHandler('distribute', distribute, pass_args = True, pass_user_data=True))
+
 
 updater.start_polling()
