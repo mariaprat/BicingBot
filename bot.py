e298ce7cd754b88b214c8d9b21a5481fd2e15d7f
mbalsells <43616135+mbalsells@users.noreply.github.com>
2019-06-12 16:20:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,6 +18,12 @@ def message(bot, update, s):
     bot.send_message(chat_id=update.message.chat_id, text=s)
 
 
+# Sends the message 's' to the user in Markdown format.
+def message_MD(bot, update, s):
+    bot.send_message(chat_id=update.message.chat_id, text=s,
+        parse_mode=telegram.ParseMode.MARKDOWN)
+
+
 # Checks if x is an integer number.
 def is_int(x):
     try:
@@ -42,33 +48,37 @@ def is_float(x):
 def start(bot, update, user_data):
     # Saves username's first name.
     name = update.message.chat.first_name
-    text = "Hello, %s! Do you need something? I'm deBOTed to" % (name) +
-           "help you." 
+    text = ("Hello, %s! Do you need something?") % (name)
+    text2 = ''' I'm deBOTed 🤖 to /help you.'''
     message(bot, update, text)
-
+    message_MD(bot, update, text2)
 
 # Gives information about the authors of the bot.
 def authors(bot, update):
-    text =  "My creators are Maria Prat and Max Balsells. Their emails are" +
-            "respectively maria.prat@est.fib.upc.edu and" +
-            "max.balsells.i@est.fib.upc.edu."
-    message(bot, update, text)
-
+    text = ('''My creators are Maria Prat and Max Balsells. 
+Their emails are respectively maria.prat@est.fib.upc.edu and 
+max.balsells.i@est.fib.upc.edu.''')
+    message_MD(bot, update, text)
 
 # Gives information about bot commands and short instructions to use them.
-def _help(bot, update):
-    text = "This bot helps you to use Bicing in Barcelona. Use /start to " +
-    "get a welcome message. Use /authors to get information about my authors." +
-    " Use /graph <distance> to initialize a new graph with up-to-date data. " +
-    "Two stations will be connected the distance between them is less than " +
-    "<distance> (in meters). Use /nodes to get the number of stations in the " +
-    "current graph. Use /edges to get the number of connections between " +
-    "stations in the current graph. Use /components to get the number of " +
-    "connected components in the current graph. Use /plotgraph to create a " +
-    "map with all the stations and connections between them. Use /route " +
-    "<origin>, <destination> to create a map with the faster route between " +
-    "<origin> and <destination>."
-    message(bot, update, text)
+def help(bot, update):
+    text = '''
+🚲 Let me help you to use *Bicing* in Barcelona! 🚲
+- Use /start to get a welcoming message. 😉
+- Use /authors to get information about my authors. 
+- Use /graph *<distance>* to initialize a new graph with up-to-date data.
+You can choose the maximum distance between two stations or take the default 
+value of 1000 m.
+- Use /nodes to get the number of stations in the current graph.
+- Use /edges to get the number of connections between stations in the current graph.
+- Use /components to get the number of connected components in the current graph.
+- Use /plotgraph to create a map 🗺️ with all the stations and connections between them. 
+- Use /route *<origin>, <destination>* to create a map with the fastest route between two addresses.
+- Use /valid\_route *<origin>, <destination>* to create a map with the fastest route between two addresses.
+The first station has at least one bike and the last one has at least one empty dock.
+'''
+    message_MD(bot, update, text)  
+
 
 
 # Tells the user how many nodes has the current graph (if previously defined).
@@ -76,7 +86,7 @@ def nodes(bot, update, user_data):
     if 'G' in user_data:
         message(bot, update, user_data['G'].number_of_nodes())
     else:
-        message(bot, update, "You must first construct the geometric graph!")
+        message(bot, update, "❗You must first construct a geometric graph!")
 
 
 # Tells the user how many connected components has the current graph
@@ -86,7 +96,7 @@ def components(bot, update, user_data):
         message(bot, update,
                 nx.number_connected_components(user_data['G']))
     else:
-        message(bot, update, "You must first construct the geometric graph!")
+        message(bot, update, "❗You must first construct a geometric graph!")
 
 
 # Tells the user how many edges has the current graph (if previously defined).
@@ -94,7 +104,7 @@ def edges(bot, update, user_data):
     if 'G' in user_data:
         message(bot, update, user_data['G'].number_of_edges())
     else:
-        message(bot, update, "You must first construct the geometric graph!")
+        message(bot, update, "❗You must first construct a geometric graph!")
 
 
 # Plots the current graph over a map (if previously defined).
@@ -111,24 +121,25 @@ def plotgraph(bot, update, user_data):
         os.remove(fitxer)
 
     else:
-        message(bot, update, "You must first construct the geometric graph!")
+        message(bot, update, "❗You must first construct a geometric graph!")
 
 
 # Defines a new geometric graph over the stations with the desired distance in
 # as meters argument. If it isn't specified, it takes 1000 m as the distance.
 # Because of the implementation, small distances (<= 5) make it eternally slow.
 def graph(bot, update, args, user_data):
-    warning_num = "You should introduce a number!"
-    warning_pos_num = "You should introduce a positive number!"
+    warning_num = "❗You should introduce a number!"
+    warning_pos_num = "❗You should introduce a positive number!"
     try:
         if (len(args) == 0):
             dist = 1000
         else:
-            dist = float(args[0])
+            dist = args[0]
 
         if (not is_float(dist)):
             message(bot, update, warning_num)
-        elif (dist <= 0):
+
+        elif (float(dist) <= 0):
             message(bot, update, warning_pos_num)
         else:
             # Import station data.
@@ -143,14 +154,16 @@ def graph(bot, update, args, user_data):
             bicing = b1[['address', 'lat', 'lon', 'capacity']]
             bikes = b2[['num_bikes_available', 'num_docks_available']]
             
-            results = geometric_graph(dist, bicing)
-            user_data['G'] = results['G']
-            user_data['position'] = results['position']
-            message(bot, update, "Graph constructed successfully!")
+            results = geometric_graph(float(dist), bicing)
+
+            user_data['G'], user_data['position'] = results
+            user_data['bikes'], user_data['bicing'] = bikes, bicing
+
+            message(bot, update, "✅ Graph constructed successfully!")
 
     except Exception as e:
         print(e)
-        text = "Unexpected error. Please, report this error to the authors 😊."
+        text = "❗Unexpected error. Please, report this error to the authors 😊."
         message(bot, update, text)
 
 
@@ -166,24 +179,24 @@ def route(bot, update, args, user_data):
                 bot.send_message(chat_id=update.message.chat_id, text=m)
             else:
                 fitxer = "route_" + str(update.message.chat_id) + ".png"
-                image = m.render(zoom=12)
+                image = m.render()
                 image.save(fitxer)
 
                 bot.send_photo(chat_id=update.message.chat_id,
                                photo=open(fitxer, 'rb'))
-                text = ("🕒 This route will take you to your destination in",
+                text = ("📍 This route will take you to your destination in "
                         "{} minutes. 🕒".format(time))
                 message(bot, update, text)
                 os.remove(fitxer)
 
         except Exception as e:
             print(e)
-            text = ("Unexpected error. Please, report this error to the",
+            text = ("❗Unexpected error. Please, report this error to the "
                     "authors 😊.")
             message(bot, update, text)
 
     else:
-        message(bot, update, "You must first construct the geometric graph!")
+        message(bot, update, "❗You must first construct a geometric graph!")
 
 
 # Returns the same as route. However, it checks that the first station has at
@@ -192,30 +205,30 @@ def valid_route(bot, update, args, user_data):
     if 'G' in user_data:
         try:
             m, time = true_route(" ".join(args), user_data['G'],
-                                 user_data['position'])
+                            user_data['position'], user_data['bikes'])
 
             if (time == -1):
                 bot.send_message(chat_id=update.message.chat_id, text=m)
             else:
                 fitxer = "valid_route_" + str(update.message.chat_id) + ".png"
-                image = m.render(zoom=12)
+                image = m.render()
                 image.save(fitxer)
 
                 bot.send_photo(chat_id=update.message.chat_id,
                                photo=open(fitxer, 'rb'))
-                text = ("🕒 This route will take you to your destination in",
+                text = ("📍 This route will take you to your destination in "
                         "{} minutes. 🕒".format(time))
                 message(bot, update, text)
                 os.remove(fitxer)
 
         except Exception as e:
             print(e)
-            text = ("Unexpected error. Please, report this error to the",
+            text = ("❗Unexpected error. Please, report this error to the "
                     "authors 😊.")
             message(bot, update, text)
 
     else:
-        message(bot, update, "You must first construct the geometric graph!")
+        message(bot, update, "❗You must first construct a geometric graph!")
 
 
 # Gets a minimum number of bikes and a minimum nomber of docks for each station
@@ -226,33 +239,33 @@ def distribute(bot, update, args, user_data):
     if 'G' in user_data:
         try:
             if (len(args) < 2):
-                message(bot, update, "You should introduce two numbers!")
+                message(bot, update, "❗You should introduce two numbers!")
             elif (not is_int(args[0] or not is_int(arg[1]))):
-                message(bot, update, "You should introduce two numbers!")
+                message(bot, update, "❗You should introduce two numbers!")
             elif (int(args[0]) < 0 or int(args[1]) < 0):
                 message(bot, update,
-                        "You should introduce two non negative numbers!")
+                        "❗You should introduce two non negative numbers!")
 
             else:
                 cost, information = distribution(int(args[0]),
                                                  int(args[1]),
                                                  user_data['G'],
-                                                 bicing,
-                                                 bikes)
+                                                 user_data['bicing'],
+                                                 user_data['bikes'])
                 if (cost > 0):
-                    text = ("Total cost of transferring bicycles:",
+                    text = ("🚛 Total cost of transferring bicycles: "
                             "{} km.".format(cost))
                     message(bot, update, text)
                 message(bot, update, information)
 
         except Exception as e:
             print(e)
-            text = ("Unexpected error. Please, report this error to the",
+            text = ("❗Unexpected error. Please, report this error to the "
                     "authors 😊.")
             message(bot, update, text)
 
     else:
-        message(bot, update, "You must first construct the geometric graph!")
+        message(bot, update, "❗You must first construct a geometric graph!")
 
 
 TOKEN = open('token.txt').read().strip()
@@ -262,7 +275,7 @@ dispatcher = updater.dispatcher
 
 # Command definitions.
 dispatcher.add_handler(CommandHandler('start', start, pass_user_data=True))
-dispatcher.add_handler(CommandHandler('help', _help))
+dispatcher.add_handler(CommandHandler('help', help))
 dispatcher.add_handler(CommandHandler('authors', authors))
 dispatcher.add_handler(CommandHandler('nodes', nodes, pass_user_data=True))
 dispatcher.add_handler(CommandHandler('edges', edges, pass_user_data=True))
